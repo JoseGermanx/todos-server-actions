dd89605e50a1c2a713a954433a5980f33de3e113
Jose Germán Martínez <jgermanmm@gmail.com>
2024-03-01 19:56:50 -0300

    Corregir importación de prisma y formato de código en la página de Todos

--- todos/app/page.tsx
+++ todos/app/page.tsx
@@ -1,41 +1,40 @@
-
 import { revalidatePath } from "next/cache";
-import  prisma  from "../lib/prisma";
+import prisma from "../lib/prisma";
 
 import Form from "../components/Form";
 
 export default async function Home() {
-
   const todos = await prisma.todo.findMany();
 
   const deleteTodo = async (formData: FormData) => {
-    'use server'
-    const id = parseInt(formData.get('id')?.toString() ?? '0');
+    "use server";
+    const id = parseInt(formData.get("id")?.toString() ?? "0");
     await prisma.todo.delete({
       where: {
         id: id as number,
       },
     });
 
-    revalidatePath('/')
-  }
-  
-  
+    revalidatePath("/");
+  };
+
   return (
     <main className="flex min-hscreen flex-col items-center w-full p-24">
       <h1 className="text-2xl font-bold">Todos Page</h1>
       <Form />
-        <ul className="list-disc">
-          {todos.map((todo) => (
-            <>
+      <ul className="list-disc">
+        {todos.map((todo) => (
+          <>
             <li key={todo.id}>{todo.label}</li>
             <form action={deleteTodo}>
-            <input type="hidden" name="id" value={todo.id} />
-            <button className="rounded bg-gray-700 text-gray-100 p-1">Delete</button>
+              <input type="hidden" name="id" value={todo.id} />
+              <button className="rounded bg-gray-700 text-gray-100 p-1">
+                Delete
+              </button>
             </form>
-            </>
-          ))}
-        </ul>
+          </>
+        ))}
+      </ul>
     </main>
   );
 }
