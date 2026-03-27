e13eefd3e5f03b61735c9d2e2c8a93246507233e
José Martínez <jgermanmm@gmail.com>
2026-03-26 22:03:56 -0300

    se agrega texrt area para que el espacio sea mas grande

--- components/Form.tsx
+++ components/Form.tsx
@@ -15,10 +15,9 @@ const Form = () => {
       }}
       className="flex flex-col w-[300px] my-16"
     >
-      <input
-        type="text"
+      <textarea
         name="label"
-        className="px-4 py-2 mb-3 border"
+        className="px-4 py-2 mb-3 border resize-none rounded-md focus:outline-none focus:ring-2 focus:ring-yellow-500 h-24 w-full"
         placeholder="Enter a new post-it..."
         required
       />
