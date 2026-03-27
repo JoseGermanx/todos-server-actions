f0db732235705eb038becac4035f5e2902441297
José Martínez <jgermanmm@gmail.com>
2026-03-26 22:16:52 -0300

    primera iteracion de las position

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,19 @@ import { revalidatePath } from "next/cache";
 import prisma from "../lib/prisma";
 
 import Form from "../components/Form";
+import Board from "@/components/Board";
 import { FaGithub } from "react-icons/fa";
 import Footer from "@/components/Footer";
-import { deleteTodo } from "@/actions/actions";
 
 export default async function Home() {
-  const todos = await prisma.todo.findMany();
+  const todos = await prisma.todo.findMany({
+    select: {
+      id: true,
+      label: true,
+      posX: true,
+      posY: true,
+    },
+  });
 
   return (
     <>
@@ -17,26 +24,7 @@ export default async function Home() {
     </a>
       <h1 className="text-2xl md:text-4xl font-bold">Post-It Page</h1>
       <Form />
-      <ul className="grid grip-cols-1 sm:grid-cols-3 md:grid-cols-4 gap-3">
-        {todos.map((todo) => (
-          <div key={todo.id} className="bg-yellow-100  drop-shadow-md rounded-md w-60 h-48">
-           <form className="mx-3 flex justify-end" action={deleteTodo}>
-                <input type="hidden" name="id" value={todo.id} />
-                <button className=" text-red-900 p-1 mb-1 mt-1 font-bold text-xs" title="Eliminar">
-                  X
-                </button>
-              </form>
-          <div
-            
-            className="p-4 bg-yellow-100"
-          >
-            <div>
-              <li className="list-none">{todo.label}</li>
-            </div>
-          </div>
-          </div>
-        ))}
-      </ul>
+      <Board todos={todos} />
     </main>
     <Footer />
 </>
